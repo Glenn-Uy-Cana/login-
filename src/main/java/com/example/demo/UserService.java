98237ff309ce452134403ad3e19067c6d3d976b3
Glenn-Uy-Cana <glennuycana@gmail.com>
2021-11-08 10:35:39 +0800

    First commit

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.example.demo.User;
+import com.example.demo.UserRegistrationDto;
+
+public interface UserService extends UserDetailsService{
+    User save(UserRegistrationDto registrationDto);
+}
